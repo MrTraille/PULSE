8a8d573ad8c65fc3e69f7e82dd562ba1496303d6
Cyril Vincent <cyril.cv20@gmail.com>
2025-11-07 14:22:41 +0100

    suite css

--- run_desktop.py
+++ run_desktop.py
@@ -3,15 +3,12 @@ import webview
 from label_ui import app
 
 def start_flask():
-    # On lance Flask en arrière-plan, sans debug ni reloader
     app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
 
 if __name__ == "__main__":
-    # Thread pour le serveur Flask
     t = threading.Thread(target=start_flask, daemon=True)
     t.start()
 
-    # Fenêtre native qui pointe vers l'app
     webview.create_window(
         "PULSE",
         "http://127.0.0.1:5000/",
